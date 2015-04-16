fd9058c673b2255c9b94686f98463cc5c5b4451d
Ye Lin Aung <me@yelinaung.com>
2015-04-16 18:25:40 +0630

    more tests according to original test.rb

--- test/rabbit_test.rb
+++ test/rabbit_test.rb
@@ -11,6 +11,7 @@ class TestRabbit < Minitest::Test
   # uni to zg converting test
   def test_convert_uni2zg
     assert_equal 'မဂၤလာပါ', Rabbit::Converter.new.uni2zg('မင်္ဂလာပါ')
+    assert_equal 'ေယဓမၼာ ေဟတုပၸဘဝါ ေတသံ ေဟတုံ တထာဂေတာ အာဟ ေတသၪၥ ေယာနိေရာေဓါ ဧဝံ ဝါဒီ မဟာသမေဏာ။', Rabbit::Converter.new.uni2zg('ယေဓမ္မာ ဟေတုပ္ပဘဝါ တေသံ ဟေတုံ တထာဂတော အာဟ တေသဉ္စ ယောနိရောဓေါ ဧဝံ ဝါဒီ မဟာသမဏော။')
   end
 
   # zg to uni converting test
@@ -18,5 +19,6 @@ class TestRabbit < Minitest::Test
     assert_equal 'မင်္ဂလာပါ', Rabbit::Converter.new.zg2uni('မဂၤလာပါ')
     assert_equal 'ဘောလုံးကန်တာမှ ကောင်းဦးမယ်', Rabbit::Converter.new.zg2uni('ေဘာလုံးကန္တာမွ ေကာင္းဦးမယ္')
     assert_equal 'မြန်မာလိုပြောမယ်လကွာ', Rabbit::Converter.new.zg2uni('ျမန္မာလိုေျပာမယ္လကြာ')
+    assert_equal 'ယေဓမ္မာ ဟေတုပ္ပဘဝါ တေသံ ဟေတုံ တထာဂတော အာဟ တေသဉ္စ ယောနိရောဓေါ ဧဝံ ဝါဒီ မဟာသမဏော။', Rabbit::Converter.new.zg2uni('ေယဓမၼာ ေဟတုပၸဘဝါ ေတသံ ေဟတုံ တထာဂေတာ အာဟ ေတသၪၥ ေယာနိေရာေဓါ ဧဝံ ဝါဒီ မဟာသမေဏာ။')
   end
 end
